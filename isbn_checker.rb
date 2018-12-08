0c2da2909a6f66c43f73d5784653ddffa679d6e3
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-12-07 23:13:05 -0500

    Check number working for 10 digit

--- isbn_checker.rb
+++ isbn_checker.rb
@@ -2,12 +2,11 @@ class Number_check
 
   def return_check_digit(number)
     n = 1
-    ary = number.split("").each_with_index.map { |x, index|
-      p index
-       x.to_i * (index + 1)
+    ary = number.split("").each_with_index.map {
+      |x, index|
+      x.to_i * (index + 1)
     }
-    p ary
-    check_digit = ary.sum%11
+    check_digit = ary.sum % 11
     return check_digit
   end
 
