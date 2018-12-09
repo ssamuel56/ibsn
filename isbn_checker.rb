2bbdf366e30b6bdfd7fbe819fc3007fe18e512cf
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-12-08 19:44:28 -0500

    Mostly finished with my class. Was still figuing out ho to pass all my variables

--- isbn_checker.rb
+++ isbn_checker.rb
@@ -1,13 +1,24 @@
-class Number_check
+class String
 
-  def return_check_digit(number)
-    n = 1
-    ary = number.split("").each_with_index.map {
+  def str
+    self.gsub(/[- \s]/, "\s" => "", "-" => "")
+  end
+
+  def return_check_digit_10()
+    ary = str.split("").each_with_index.map {
       |x, index|
-      x.to_i * (index + 1)
+      index != 9 ? x.to_i * (index + 1) : 0
     }
-    check_digit = ary.sum % 11
+    check_digit = ary.sum() % 11
     return check_digit
+  end
+
+  def verify_number()
+    puts return_check_digit_10
+    puts str[-1]
+    valid = true if return_check_digit_10() == str[-1].to_i
+    valid = true if return_check_digit_10() == 10 && str[-1] == 'x'
+    return valid
   end
 
 end
--- isbn_checker_tdd.rb
+++ isbn_checker_tdd.rb
@@ -3,13 +3,15 @@ require 'minitest/autorun'
 
 class Test_for_isbn < Minitest::Test
 
-  def setup
-    @function = Number_check.new
+  def test_for_return_check_digit_10
+    assert_equal(7, "0471958697".return_check_digit_10)
+    assert_equal(0, "0321146530".return_check_digit_10)
+    assert_equal(10, "877195869x".return_check_digit_10)
   end
 
-  def test_for_return_check_digit
-    number = "047195869"
-    assert_equal(7, @function.return_check_digit(number))
+  def test_for_verify_number
+    assert_equal(true, "877195869x".verify_number)
+    assert_equal(true, "0-321-14653-0".verify_number)
   end
 
 end
