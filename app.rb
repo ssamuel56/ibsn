e134e3d221eafdc2462d1f2b5470efd044085d30
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-12-12 14:34:40 -0500

    Added option to upload csv file.

--- app.rb
+++ app.rb
@@ -3,10 +3,17 @@ require_relative 'isbn_checker.rb'
 
 get '/' do
   number = params[:number]
-  erb :home, locals: {number: number}
+  file = params[:file]
+  erb :home, locals: {number: number, file: file}
 end
 
 post '/check' do
   isbn = params[:isbn]
   redirect '/?number=' + isbn
+end
+
+post '/file' do
+  isbn = params[:isbn]
+  isbn = isbn_file_maker(isbn)
+  redirect '/?file=' + isbn
 end
